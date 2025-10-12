b3ea81e0828fad450d8169544b79b2f93f5fea54
alaaadeln7 <alaaadeln7@gmail.com>
2025-10-12 03:31:26 +0300

    handle path in mial module

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -21,7 +21,7 @@ import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
             },
           },
           template: {
-            dir: join(__dirname, 'templates'),
+            dir: join(__dirname, '..', 'mail', 'templates'),
             adapter: new EjsAdapter({
               inlineCssEnabled: true,
             }),
